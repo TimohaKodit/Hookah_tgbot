8b82a5c4024b5dbcfdee1f5cc06867fe5fa92fc3
TimohaKodit <63901163+TimohaKodit@users.noreply.github.com>
2024-06-05 21:44:53 +0500

    Update config.py

--- hookah/apps/config/config.py
+++ hookah/apps/config/config.py
@@ -1,9 +1,9 @@
 from aiogram import Bot
 
-TOKEN = '7001878261:AAHRWuzHxiCLYXpxDH-yEHgkc1-3kb0FQjQ'
+TOKEN = 'Your Telegram bots token'
 #1003429843
-ADMIN_ID= [351961407]
+ADMIN_ID= []
 #351961407
 SQLALCHEMY_URL = 'sqlite+aiosqlite:///./database.db'
 
-HOOKAH_MASTER_ID = 1003429843
+HOOKAH_MASTER_ID = 
